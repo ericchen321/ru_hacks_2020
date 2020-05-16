0447bc359c4611a1d24e0bdec59d450dcf31681b
DanielTongAwesome <danieltongubc@gmail.com>
2020-05-15 22:36:01 -0700

    [#2] able to check the zoom camera status

--- zoommate.py
+++ zoommate.py
@@ -1,15 +1,31 @@
 import psutil
 
-# target - process name / app name
+# check tartget(app) status
 def checkAppStatus(target):
     # return true if the process is running
     return "zoom.us" in (p.name() for p in psutil.process_iter())
 
+# check target(app) thread number
+def checkAppThreadNumber(target):
+    for p in psutil.process_iter():
+        if p.name() == target:
+            return p.num_threads()
+        else:
+            continue 
 
+def checkZoomVideoStatus(target):
+    while(True):
+        if checkAppStatus(target) == True:
+            thread_num = checkAppThreadNumber(target)
+            print("{} is running, it has {} threads ".format(target, thread_num))
+            if int(thread_num) > 32:
+                print("Thread number bigger than threshold, the camera is on .....")
+            else:
+                print("Thread number is below the threshold")
+        else:
+            continue            
 
 
 if __name__ == "__main__":
-    app_status = checkAppStatus('zoom.us')
-    print(app_status)
-
-
+    checkZoomVideoStatus('zoom.us')
+   
